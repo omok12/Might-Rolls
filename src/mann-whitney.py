2877ef77e33c1a5891053d2267efdf483ad03842
O <olivermok@gmail.com>
2020-04-08 11:16:24 -0700

    added mann-whitney function, and for loop to compare consecutive levels

--- src/mann-whitney.py
+++ src/mann-whitney.py
@@ -1,5 +1,6 @@
 import scipy.stats as stats
 from src.use_this import use_this_df
+import pandas as pd
 
 df = use_this_df()
 
@@ -9,7 +10,25 @@ df_lvl_10 = df[df['Level'] == 10]
 lvl_2_mod = df_lvl_2['modifier']
 lvl_10_mod = df_lvl_10['modifier']
 
-# h0 - the modifiers at level 10 are equally likely to be higher than modifiers at level 2 as the other way around
+# h0-the modifiers at level 'sample1' are equally likely to be higher than modifiers at level 'sample2' as the other way around
 
-res = stats.mannwhitneyu(lvl_10_mod, lvl_2_mod, alternative='greater')
-print(res.pvalue)
+
+def mann_whitney_test(sample1, sample2, i):
+    res = stats.mannwhitneyu(sample1, sample2, alternative='greater').pvalue
+    print(f"p-value for level{i+2} < level{i+3}: {res}")
+    return res
+
+samples = []
+for i in range(2,12):
+    samples.append(df[df['Level'] == i]['modifier'])
+
+for i in range(0,9):
+    mann_whitney_test(samples[i], samples[i+1], i)
+
+
+
+
+
+
+
+
