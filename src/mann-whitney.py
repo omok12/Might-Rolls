77917824e9dcbc5b28bb0d05bba82ee89bd58737
O <olivermok@gmail.com>
2020-04-08 14:14:07 -0700

    added mann-whitney table

--- src/mann-whitney.py
+++ src/mann-whitney.py
@@ -2,31 +2,36 @@ import scipy.stats as stats
 from src.use_this import use_this_df
 import pandas as pd
 
+
 df = use_this_df()
-
-df_lvl_2 = df[df['Level'] == 2]
-df_lvl_10 = df[df['Level'] == 10]
-
-lvl_2_mod = df_lvl_2['modifier']
-lvl_10_mod = df_lvl_10['modifier']
 
 # h0-the modifiers at level 'sample1' are equally likely to be higher than modifiers at level 'sample2' as the other way around
 
 
-def mann_whitney_test(sample1, sample2, i):
-    res = stats.mannwhitneyu(sample1, sample2, alternative='greater').pvalue
-    print(f"p-value for level{i+2} < level{i+3}: {res}")
-    return res
+def mann_whitney_test(level=2):
+    samples = []
+    for i in range(2, 12):
+        samples.append(df[df['Level'] == i]['modifier'])
 
-samples = []
-for i in range(2,12):
-    samples.append(df[df['Level'] == i]['modifier'])
-
-for i in range(0,9):
-    mann_whitney_test(samples[i], samples[i+1], i)
-
+    p_vals = []
+    for i in range(0, 10):
+        res = stats.mannwhitneyu(samples[level-2], samples[i], alternative='greater').pvalue.round(3)
+        p_vals.append(res)
+    return p_vals
 
 
+# samples = []
+# for i in range(2, 12):
+#     samples.append(df[df['Level'] == i]['modifier'])
+# for i in range(0, 9):
+#     res = stats.mannwhitneyu(samples[i], samples[i + 1], alternative='greater').pvalue
+#     print(f"p-value for level{i + 2} < level{i + 3}: {res}")
+
+
+data = [mann_whitney_test(x) for x in range(2,12)]
+df_pval = pd.DataFrame(data, index=range(2,12), columns=range(2,12))
+print(df_pval)
+print(mann_whitney_test(2))
 
 
 
